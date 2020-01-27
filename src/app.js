0773982dcd8588d724567d7bf075122f53e72cd9
guy.batton <guy.batton@duckcreek.com>
2020-01-26 22:01:10 -0600

    got through module 1 task 3

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import fs from 'fs'
+import path from 'path'
+import express from 'express'
+
+const app = express()
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use(express.static(path.join(__dirname, 'public')))
