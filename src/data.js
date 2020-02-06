57a17949b08f6e4129e97b8a7d2d68ee6913178a
guy.batton <guy.batton@duckcreek.com>
2020-02-05 19:29:42 -0600

    finished data.js

--- src/data.js
+++ src/data.js
@@ -9,3 +9,10 @@ const accounts = JSON.parse(accountData)
 const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), { encoding: 'UTF8' })
 const users = JSON.parse(userData)
 
+const writeJSON = () => fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), JSON.stringify(accounts, null, 4), 'utf8')
+
+module.exports = {
+  accounts,
+  users,
+  writeJSON
+}
